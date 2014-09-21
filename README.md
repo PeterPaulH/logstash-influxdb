75cafd246f966f157403e2aa6055ca48fba9b8dc
PeterPaulH <peter.paul.holzhauer@ibridge.nl>
2014-09-21 20:13:09 +0200

    Initial commit

--- README.md
+++ README.md
@@ -1,4 +1,39 @@
 logstash-influxdb
 =================
 
-Logstash output for InfluxDB
+Logstash output for InfluxDB based on the existing udp output.
+
+* Installation 
+Place the file src/influxdb.rb into the folder lib/logstash/outputs
+
+* InfluxDB-settings
+Somewhere in the config.toml file:
+
+[input_plugins]
+  [input_plugins.udp]
+    enabled = true
+    port = 4444
+    database = "<database-name>"
+
+* Logstash-settings
+
+output {
+  ...
+  influxdb {
+    host     => "<hostname or IP of InfluxDB-server>"
+    port     => <port of udp-inputs.plugin of InfluxDB-server
+    name     => [ "AM", "%{attributename}" ]
+    columns  => [ "otap", "hostname", "resourcename", "value" ]
+    points   => [ "%{otap}", "c"
+                , "%{hostname}", "c"
+                , "%{resourcename}", "c"
+                , "%{num_value}", "f" ]
+  }
+  ...
+}
+
+Name:
+
+Columns:
+
+Points:
---
+++ src/influxdb.rb
@@ -0,0 +1,70 @@
+# encoding: utf-8
+require "logstash/outputs/base"
+require "logstash/namespace"
+require "socket"
+
+# Send events over UDP to InfluxDB
+#
+# Keep in mind that UDP will lose messages.
+class LogStash::Outputs::InfluxDB < LogStash::Outputs::Base
+  config_name "influxdb"
+  milestone 1
+  
+  # --- Config settings
+  # host    The address to send messages to
+  # port    The port to send messages on
+  # name    The InfluxDB serie to send data to
+  # columns
+  # point
+  config :host,     :validate => :string, :required => true
+  config :port,     :validate => :number, :required => true
+  config :name,     :validate => :array,  :required => true
+  config :columns,  :validate => :array,  :required => true
+  config :points,   :validate => :hash,   :required => true
+
+  public
+  def register
+    @socket = UDPSocket.new
+  end
+
+  def receive(event)
+    return unless output?(event)
+    if event == LogStash::SHUTDOWN
+      finished
+      return
+    end
+    # --- Example
+    # [ {
+    #     "name" : "hd_used",
+    #     "columns" : ["value", "host", "mount"],
+    #     "points" : [ [23.2, "serverA", "/mnt"] ]
+    #   } ]
+    @logger.debug("INFLUXDB: Start processing ...")
+    aName = []
+    aCols = []
+    aPnts  = []
+    @name.each do |nm|
+        aName << event.sprintf(nm)
+    end
+    @columns.each do |col|
+      aCols << '"' + event.sprintf(col) + '"'
+    end
+    @points.each do |point,type|
+      if type == 'n'
+        aPnts << event.sprintf(point)
+      elsif type == 'f'
+        aPnts << event.sprintf(point).to_f
+      else
+        aPnts << '"' + event.sprintf(point) + '"'
+      end
+    end
+    cName = aName.join(".")
+    cCols = aCols.join(",")
+    cPnts = aPnts.join(",")
+    cMessage = '[ { "name" : "' + cName + '", "columns" : [' + cCols + '], "points" : [[' + cPnts + ']]}]' + "\n"
+    @logger.debug("INFLUXDB: sending ", :host => @host, :port => @port, :txtmessage => cMessage )
+
+    @socket.send(cMessage, 0, @host, @port)
+  end
+
+end # class LogStash::Outputs::InfluxDB
